83bf8b7097fd12a9ccb353a488aca3462ef1a7f9
Kabeer Akande <kabeer.akande@ovo.com>
2023-06-03 11:58:11 +0100

    add functionalities to run module and test together

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -5,3 +5,8 @@ import calc
 class TestCalc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(2,3), 5)
+        self.assertEqual(calc.add(2,3), 5.5)
+
+if __name__ ==  "__main__":
+    unittest.main()
+
